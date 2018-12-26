89a6bd76eea40b63654ff8a33ad76d2804bfb17f
Keisuke Watanabe <wtnbass@icloud.com>
2018-12-26 10:53:50 +0900

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -47,69 +47,7 @@ function Test-Dir {
 }
 
 # HTTPサーバーを起動する。
-# TODO: ctrl+Cの後にもう一度アクセスしないと終了しない
-function Simple-HttpServer {
-    param(
-        $Port=8000,
-        [switch] $Open=$False
-    )
-
-    function Test-Should-Add-Slash {
-      param(
-          $fullPath,
-          $localUrl
-      )
-      return ((Test-Dir $fullPath) -and ($localUrl[$localUrl.length - 1] -ne "/"))
-    }
-
-    $parentPath = $pwd.path
-    $url = "http://localhost:$Port/";
-
-    try {
-        $listener = New-Object System.Net.HttpListener
-        $listener.Prefixes.Add($url)
-        $listener.Start()
-        Write-Host "Start $url"
-
-        if ($Open) {
-          start $url
-        }
-
-        while($True) {
-            $context = $listener.GetContext()
-            $requestUrl = $context.Request.Url
-            $response = $context.Response
-
-            $fullPath = Join-Path $parentPath $requestUrl.LocalPath
-
-            if (Test-Should-Add-Slash $fullPath $requestUrl.LocalPath) {
-                $response.Redirect($requestUrl.AbsolutePath + "/")
-                $response.Close()
-                continue
-            }
-
-            if (Test-Dir $fullPath) {
-                $fullPath = Join-Path  $fullPath "index.html"
-            }
-
-            if([IO.File]::Exists($fullPath)) {
-                if ($fullpath -match '\.m?js$') {
-                  $response.addHeader("Content-Type", "text/javascript")
-                }
-
-                $buffer = [IO.File]::ReadAllBytes($fullPath)
-                $response.ContentLength64 = $buffer.Length
-                $response.OutputStream.Write($buffer, 0, $buffer.Length)
-
-            } else {
-                $response.StatusCode = 404
-            }
-
-            Write-Host $response.StatusCode $requestUrl.LocalPath
-
-            $response.Close()
-        }
-    } finally {
-        $listener.Stop()
-    }
+$HttpServer = Resolve-Path $PROFILE/../HttpServer.ps1
+function Start-Server {
+  start powershell -ArgumentList $HttpServer
 }
